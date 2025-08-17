d4ea55f2af01ab5665f76dcaff3a3f14d162275c
Glaucia Marks <glaucione.mg@gmail.com>
2025-08-16 23:15:54 -0300

    Implementacao do projeto smartPhone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,29 @@
 ﻿using DesafioPOO.Models;
+ 
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine($"Digite o numero");
+string numeroDigitado = Console.ReadLine();
+
+Console.WriteLine($"Digite o modelo");
+string modeloDigitado = Console.ReadLine();
+
+Console.WriteLine($"Digite o imei");
+string imeiDigitado = Console.ReadLine();
+
+Console.WriteLine($"Digite a memoria (apenas numeros)");
+int memoriaDigitada = Convert.ToInt32(Console.ReadLine());
+
+if (modeloDigitado == "nokia")
+{
+    Nokia nokia = new Nokia(numeroDigitado, modeloDigitado, imeiDigitado, memoriaDigitada);
+
+    nokia.InstalarAplicativo("App Nokia");
+}
+
+if (modeloDigitado == "iphone")
+{
+    Iphone iphone = new Iphone(numeroDigitado, modeloDigitado, imeiDigitado, memoriaDigitada);
+
+    iphone.InstalarAplicativo("Iphone App");
+}
+
